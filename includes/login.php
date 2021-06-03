01e4340cd4ea01910c7b8d10c054e2113afce844
Mostafa <ubirockteam@gmail.com>
2021-06-04 02:59:18 +0430

    minor url changes

--- includes/login.php
+++ includes/login.php
@@ -35,42 +35,43 @@ session_start();
 
     <div class="container text-center">
         <div class="well">
-        <?php
+            <?php
 
-if (isset($_POST['submit'])) {
+            if (isset($_POST['submit'])) {
+                
+                $username = mysqli_real_escape_string($conn, $_POST['username']);
+                $password = mysqli_real_escape_string($conn, $_POST['password']);
+                $query = "SELECT * FROM users WHERE username='{$username}' AND user_password = '{$password}'";
+                // echo $query;
+                $result = mysqli_query($conn, $query);
+                if (!$result) {
+                    echo "result bug";
+                } else if (empty($result)) {
+                    echo "result is empty";
+                } else if (mysqli_num_rows($result) == 0) {
+                    echo "<div class='alert alert-danger'>The account name or password that you have entered is incorrect</div>";
+                    // sleep(3);
+                    // header('location:../index.php');
+                    // header("refresh:2; url=../index.php");
+                } else {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $_SESSION['user_id'] = $row['user_id'];
+                        $_SESSION['username'] = $row['username'];
+                        $_SESSION['email'] = $row['user_email'];
+                        $_SESSION['role'] = $row['user_role'];
+                        $_SESSION['firstname'] = $row['user_firstname'];
+                        $_SESSION['lastname'] = $row['user_lastname'];
+                        $_SESSION['password'] = $password;
+                        // $_SESSION['last_time'] = time();
 
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
-    $query = "SELECT * FROM users WHERE username='{$username}' AND user_password = '{$password}'";
-    // echo $query;
-    $result = mysqli_query($conn, $query);
-    if (!$result) {
-        echo "result bug";
-    } else if (empty($result)) {
-        echo "result is empty";
-    } else if (mysqli_num_rows($result) == 0) {
-        echo "<div class='alert alert-danger'>The account name or password that you have entered is incorrect</div>";
-        // sleep(3);
-        // header('location:../index.php');
-        // header("refresh:2; url=../index.php");
-    } else {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $_SESSION['user_id'] = $row['user_id'];
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['email'] = $row['user_email'];
-            $_SESSION['role'] = $row['user_role'];
-            $_SESSION['firstname'] = $row['user_firstname'];
-            $_SESSION['lastname'] = $row['user_lastname'];
-            // $_SESSION['last_time'] = time();
 
-            
-            header('location: ../admin');
-            // $_SESSION['username'] = $row['username'];
-        }
-        // echo $_SESSION['username'] , $_SESSION['role'];
-    }
-}
-?>
+                        header('location: ../admin');
+                        // $_SESSION['username'] = $row['username'];
+                    }
+                    // echo $_SESSION['username'] , $_SESSION['role'];
+                }
+            }
+            ?>
             <h1>Login Page</h1>
             <form method="POST" action="./loginprocess.php">
                 <div class="form-group">
@@ -83,8 +84,8 @@ if (isset($_POST['submit'])) {
                     <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <!-- <div class="form-check"> -->
-                    <!-- <input type="checkbox" class="form-check-input" id="exampleCheck1"> -->
-                    <!-- <label class="form-check-label" for="exampleCheck1">Check me out</label> -->
+                <!-- <input type="checkbox" class="form-check-input" id="exampleCheck1"> -->
+                <!-- <label class="form-check-label" for="exampleCheck1">Check me out</label> -->
                 <!-- </div> -->
                 <button name="submit" type="submit" class="btn btn-primary">Submit</button>
             </form>
